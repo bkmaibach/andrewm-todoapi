ae585b108f72d47cdc5efcfad5f3b7783178f9e7
Brad Maibach <bmaibach@gmail.com>
2018-07-02 14:21:20 -0700

    Added tests for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,40 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = testTodos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).to.equal(hexId);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);f
+            }
+            //Query database by id, and expect that the item does not exist, using the to not exist assertion
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).to.be.null;
+                done();
+            }).catch((e) => done(e));
+        });
+
+    });
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete(`/todos/whatisthisanidforidiots}`)
+        .expect(404)
+        .end(done);
+    });
+});
